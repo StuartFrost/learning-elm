178fd5b9875df78ffc6ccca427eafeb26d4230b3
Stuart Frost <stuart.2.frost@bt.com>
2018-02-20 19:37:49 +0000

    01 function exercises done - could do with a refactor

---
+++ exercises/01_functions/Exercise2.elm
@@ -0,0 +1,18 @@
+module Main exposing (..)
+
+import Html
+
+
+uppercase_longer_than_10 name =
+    if String.length name > 10 then
+        String.toUpper name
+    else
+        name
+
+
+main =
+    let
+        name =
+            "Stuart Frost"
+    in
+        Html.text (String.append (String.append (uppercase_longer_than_10 name) " - name length: ") (toString (String.length name)))
